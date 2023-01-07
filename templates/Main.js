350157c8f1bdbf0a0d4eba58117d706953d60230
Sjinga Awal <71959074+Seadoge-keeper@users.noreply.github.com>
2023-01-08 01:02:23 +0400

    Update Main.js

--- templates/Main.js
+++ templates/Main.js
@@ -1,24 +1,21 @@
-const testing = document.querySelector("#test");
-
-var feedBox = document.querySelector("#NewsFeed")
+var feedBox = document.querySelector("#NewsFeed") 
 
 async function getData() {
     const response = await fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=GCCyf0tGEmqZurie42bVGVnDghWI8tX6')
-    const data = await response.json();
-    console.log(data.results)
-    for (i = 0; i < 30; i++) {
+    const data = await response.json(); // Stores the response into a JSON file labelled "data"
+    console.log(data.results) //outputs the table of "results" inside the newly obtained data.
+    for (i = 0; i < 30; i++) { // For loop
         var li = document.createElement('li'); //Create List item container
         var newsHead = document.createElement('h2')// holder for the headline
         var a = document.createElement('a'); //holder for the abstract
 
-        newsHead.innerText = data.results[i].title;
-        a.innerText = data.results[i].abstract;
-        a.href = data.results[i].url
+        newsHead.innerText = data.results[i].title; // inputs the title of the article into the news Headline
+        a.innerText = data.results[i].abstract; // inputs the abstract into the innerText of the <a> tag
+        a.href = data.results[i].url // inputs the URL of the article into the <a> tag
 
-        feedBox.append(li)
-        li.appendChild(newsHead)
-        li.appendChild(a)
-        li.app
+        feedBox.append(li) // This adds a new List item into the NewsFeed List 
+        li.appendChild(newsHead) // this will add a new heading tag inside of the List item
+        li.appendChild(a) // This will add an <a> tag containing the abstract of the article and URL
         console.log(data.results[i].title,
             "\n", data.results[i].abstract,
             "\n", data.results[i].url)
@@ -26,11 +23,5 @@ async function getData() {
 }
 
 getData();
-
-console.log(testing + "Console");
-
-testing.addEventListener("click", event => {
-    
-})
 
 
